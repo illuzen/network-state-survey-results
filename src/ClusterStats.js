4ec0f7523ca2b60ff83c532e557b1225acd502ea
illuzen <snakecharmr1024@gmail.com>
2024-03-28 03:52:48 +0000

    Updated chart

--- src/ClusterStats.js
+++ src/ClusterStats.js
@@ -37,10 +37,13 @@ function ClusterStats(props) {
             labels: labels,
             datasets: [{
                 data: labels.map(label => item[label]),
-                backgroundColor: 'rgba(99, 199, 221, 0.4)',
-                // backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderColor: 'transparent',
             }],
         };
     }
@@ -55,10 +58,25 @@ function ClusterStats(props) {
         scales: {
             x: {
                 type: 'category',
+                grid: {
+                    color: 'transparent',
+                },
             },
             y: {
                 type: 'linear',
                 beginAtZero: true,
+                grid: {
+                    color: 'transparent',
+                },
+                ticks: {
+                    stepSize: 1,
+                    callback: function(value) {
+                        if (Number.isInteger(value)) {
+                            return value.toString();
+                        }
+                        return value.toFixed(1);
+                    }
+                }
             }
         },
         responsive: true,
